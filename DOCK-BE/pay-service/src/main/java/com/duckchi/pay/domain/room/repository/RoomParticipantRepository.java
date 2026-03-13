70eb842e9d9f5b45db039b8d9fe977addcf4f665
ImChanHyuck <ibb61@naver.com>
2026-03-13 11:28:35 +0900

    [BE/Feat]invite_entit_repo구현

--- DOCK-BE/pay-service/src/main/java/com/duckchi/pay/domain/room/repository/RoomParticipantRepository.java
+++ DOCK-BE/pay-service/src/main/java/com/duckchi/pay/domain/room/repository/RoomParticipantRepository.java
@@ -4,4 +4,6 @@ import com.duckchi.pay.domain.room.entity.RoomParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomParticipantRepository extends JpaRepository<RoomParticipant, Long> {
+
+    boolean existsByRoom_IdAndUserId(Long roomId, Long userId);
 }
