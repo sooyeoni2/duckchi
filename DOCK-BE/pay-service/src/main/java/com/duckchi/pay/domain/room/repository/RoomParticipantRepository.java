7bb375763d3024af3983b720bc1b066f731ba629
ImChanHyuck <ibb61@naver.com>
2026-03-16 11:20:37 +0900

    [BE/Feat] 자동이체 동의/거절 api repo,res,type 구현

--- DOCK-BE/pay-service/src/main/java/com/duckchi/pay/domain/room/repository/RoomParticipantRepository.java
+++ DOCK-BE/pay-service/src/main/java/com/duckchi/pay/domain/room/repository/RoomParticipantRepository.java
@@ -1,9 +1,12 @@
 package com.duckchi.pay.domain.room.repository;
 
 import com.duckchi.pay.domain.room.entity.RoomParticipant;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomParticipantRepository extends JpaRepository<RoomParticipant, Long> {
 
     boolean existsByRoom_IdAndUserId(Long roomId, Long userId);
+
+    Optional<RoomParticipant> findByRoom_IdAndUserId(Long roomId, Long userId);
 }
