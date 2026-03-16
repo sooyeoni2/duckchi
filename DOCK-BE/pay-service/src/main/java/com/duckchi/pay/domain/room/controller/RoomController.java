f43c85a50038c4e532642553711baf24293e958b
ImChanHyuck <ibb61@naver.com>
2026-03-16 11:17:25 +0900

    [BE/Feat] 자동이체 동의/거절 api 컨트롤러 구현

--- DOCK-BE/pay-service/src/main/java/com/duckchi/pay/domain/room/controller/RoomController.java
+++ DOCK-BE/pay-service/src/main/java/com/duckchi/pay/domain/room/controller/RoomController.java
@@ -2,7 +2,9 @@ package com.duckchi.pay.domain.room.controller;
 
 import com.duckchi.pay.domain.room.dto.request.CreateRoomRequest;
 import com.duckchi.pay.domain.room.dto.response.CreateRoomResponse;
+import com.duckchi.pay.domain.room.dto.response.UpdateAutoDebitConsentResponse;
 import com.duckchi.pay.domain.room.service.RoomService;
+import com.duckchi.pay.domain.room.type.AutoDebitConsentStatus;
 import com.duckchi.pay.global.response.ApiResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,10 +12,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,5 +37,16 @@ public class RoomController {
     ) {
         CreateRoomResponse response = roomService.createRoom(currentUserId, request);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponseDto.success(response));
+    }
+
+    @PatchMapping("/{roomId}/auto-debit/consents")
+    @Operation(summary = "ROOM-04 자동이체 동의/거절")
+    public ResponseEntity<ApiResponseDto<UpdateAutoDebitConsentResponse>> updateAutoDebitConsent(
+            @PathVariable Long roomId,
+            @RequestParam AutoDebitConsentStatus status,
+            @RequestHeader(value = "X-User-Id", required = false) Long currentUserId
+    ) {
+        UpdateAutoDebitConsentResponse response = roomService.updateAutoDebitConsent(roomId, currentUserId, status);
+        return ResponseEntity.ok(ApiResponseDto.success(response));
     }
 }
