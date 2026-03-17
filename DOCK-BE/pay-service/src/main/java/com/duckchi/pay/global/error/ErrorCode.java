3e388675fe8c6fa12d62e73e051931c55e24ca48
정우주 <goqkfkrltl12@naver.com>
2026-03-17 11:56:12 +0900

    [BE/Style] ErrorCode 세미콜론 수정

--- DOCK-BE/pay-service/src/main/java/com/duckchi/pay/global/error/ErrorCode.java
+++ DOCK-BE/pay-service/src/main/java/com/duckchi/pay/global/error/ErrorCode.java
@@ -15,7 +15,7 @@ public enum ErrorCode {
 
     // FINANCE (Pay Service)
     FINANCE_API_ERROR("FINANCE-502-1", HttpStatus.BAD_GATEWAY, "외부 금융 시스템과의 통신 중 오류가 발생하였습니다."),
-    FINANCE_INVALID_ACCOUNT("FINANCE-400-1", HttpStatus.BAD_REQUEST, "유효하지 않은 계좌 정보입니다.");
+    FINANCE_INVALID_ACCOUNT("FINANCE-400-1", HttpStatus.BAD_REQUEST, "유효하지 않은 계좌 정보입니다."),
 
 
 
