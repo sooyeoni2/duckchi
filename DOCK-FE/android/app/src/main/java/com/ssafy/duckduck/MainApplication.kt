3cb3643ce036711c538b2b7662a4a3b76b284cab
정우주 <goqkfkrltl12@naver.com>
2026-03-19 17:47:12 +0900

    [FE/Chore] 안드로이드 FCM 개발 환경 설정

--- DOCK-FE/android/app/src/main/java/com/ssafy/duckduck/MainApplication.kt
+++ DOCK-FE/android/app/src/main/java/com/ssafy/duckduck/MainApplication.kt
@@ -1,23 +1,31 @@
-package com.frontend
+package com.ssafy.duckduck
 
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactHost: ReactHost by lazy {
-    getDefaultReactHost(
-      context = applicationContext,
-      packageList =
+  override val reactNativeHost: ReactNativeHost =
+    object : DefaultReactNativeHost(this) {
+      override fun getPackages() =
         PackageList(this).packages.apply {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // add(MyReactNativePackage())
-        },
-    )
+        }
+
+      override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+
+      override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+    }
+
+  override val reactHost: ReactHost by lazy {
+    getDefaultReactHost(applicationContext, reactNativeHost)
   }
 
   override fun onCreate() {
--- DOCK-FE/app.config.js
+++ DOCK-FE/app.config.js
@@ -6,6 +6,10 @@ const appJson = require('./app.json');
 module.exports = {
   expo: {
     ...appJson.expo,
+    "plugins": [
+    "@react-native-firebase/app",
+    "@react-native-firebase/messaging"
+  ],
     extra: {
       kakaoClientId: process.env.KAKAO_CLIENT_ID,
       kakaoRedirectUri: process.env.KAKAO_REDIRECT_URI,
