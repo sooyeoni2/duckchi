c54d4231d7848ddc0a3fa72f40bb7575d9c6f0c5
ryubyeongsun <133829802+ryubyeongsun@users.noreply.github.com>
2026-03-20 13:32:03 +0900

    [INFRA/Fix] 운영 도메인 기준 API 및 Swagger 주소 설정 반영

--- DOCK-FE/src/core/constants/apiConstants.ts
+++ DOCK-FE/src/core/constants/apiConstants.ts
@@ -1,6 +1,6 @@
 import Constants from 'expo-constants';
 
-export const API_BASE_URL = 'http://192.168.100.139:8081';
+export const API_BASE_URL = 'http://j14c102.p.ssafy.io';
 export const API_TIMEOUT = 10000;
 
 export const ENDPOINTS = {
