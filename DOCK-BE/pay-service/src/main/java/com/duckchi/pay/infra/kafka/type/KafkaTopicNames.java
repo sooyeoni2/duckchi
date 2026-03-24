2c9c9376d6699e88946a1de0426d0190c3c58221
정우주 <goqkfkrltl12@naver.com>
2026-03-24 15:03:03 +0900

    [BE/Feat] KafkaOutbox Pattern 적용 공통 설정 추가

---
+++ DOCK-BE/pay-service/src/main/java/com/duckchi/pay/infra/kafka/type/KafkaTopicNames.java
@@ -0,0 +1,11 @@
+package com.duckchi.pay.infra.kafka.type;
+
+public final class KafkaTopicNames {
+
+    //모임방 시작,종료 이벤트 토픽 이름
+    public static final String ROOM_LIFECYCLE_NOTIFICATION_EVENT = "room-lifecycle-notification-event";
+
+    private KafkaTopicNames(){
+
+    }
+}
