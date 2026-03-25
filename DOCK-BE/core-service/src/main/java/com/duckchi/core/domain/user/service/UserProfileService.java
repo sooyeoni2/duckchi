0677784c6755e99faeb38dfaf8a7f6f84f1ff9e8
김수연 <rtd710@naver.com>
2026-03-25 11:15:54 +0900

    -[BE/Fix] 알림 설정 온오프 관련 코드 삭제

--- DOCK-BE/core-service/src/main/java/com/duckchi/core/domain/user/service/UserProfileService.java
+++ DOCK-BE/core-service/src/main/java/com/duckchi/core/domain/user/service/UserProfileService.java
@@ -1,6 +1,5 @@
 package com.duckchi.core.domain.user.service;
 
-import com.duckchi.core.domain.user.dto.request.NotificationSettingRequest;
 import com.duckchi.core.domain.user.dto.request.UserProfileEditRequest;
 import com.duckchi.core.domain.user.dto.request.UserProfileImageEditRequest;
 import com.duckchi.core.domain.user.dto.response.UserProfileDetailResponse;
@@ -14,5 +13,4 @@ public interface UserProfileService {
 
     UserProfileEditResponse editProfileImage(Long userId, UserProfileImageEditRequest request);
 
-    void updateNotificationSetting(Long userId, NotificationSettingRequest request);
 }
