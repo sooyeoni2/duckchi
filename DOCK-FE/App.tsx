99b3a397a524e992b1faa448aa73ed31764525ee
강산천 <rkdtkscjs1@naver.com>
2026-03-27 00:23:00 +0900

    [FE/Feat] 글로벌 Toast 알림 시스템 구현 및 정적 Alert 전환

--- DOCK-FE/App.tsx
+++ DOCK-FE/App.tsx
@@ -40,6 +40,7 @@ import { PayPasswordConfirmScreen } from './src/features/bank/views/PayPasswordC
 import { PayPasswordInputScreen } from './src/features/bank/views/PayPasswordInputScreen';
 import { PayPasswordSetupScreen } from './src/features/bank/views/PayPasswordSetupScreen';
 import { OnboardingScreen } from './src/features/onboarding/OnboardingScreen';
+import { GlobalToast } from './src/shared/components/GlobalToast';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -385,6 +386,7 @@ function App() {
         onPress={handleForegroundBannerPress}
         onClose={handleForegroundBannerClose}
       />
+      <GlobalToast />
     </SafeAreaProvider>
   );
 }
